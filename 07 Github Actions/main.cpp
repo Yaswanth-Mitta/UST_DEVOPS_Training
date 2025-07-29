49b8d0d856a2b6150fc91a8b317c0b567724ec0e
Yaswanth Mitta <yaswanthmitta@gmail.com>
2025-07-29 17:31:40 +0530

    workflows with docker file and pusing image to docker hub with multi stage builds

---
+++ 07 Github Actions/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World from Docker which is pushed to docker hub using github actions and using multi stage builds!" << std::endl;
+    return 0;
+}
